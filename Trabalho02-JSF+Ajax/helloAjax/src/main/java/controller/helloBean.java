1e1c1a3dbede8bcc2abbada843d8c28b6aff2157
mikaelstainczyk0109 <84359013+mikaelstainczyk0109@users.noreply.github.com>
2021-06-01 22:52:28 -0300

    Atualização no projeto AJAX
    Alteração da aplicação JSF + AJAX com a inclusão de mais dois campos no
    formulário de entrada (input)

--- Trabalho02-JSF+Ajax/helloAjax/src/main/java/controller/helloBean.java
+++ Trabalho02-JSF+Ajax/helloAjax/src/main/java/controller/helloBean.java
@@ -8,15 +8,20 @@ package controller;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.Date;
 
 @Named(value = "helloBean")
 @SessionScoped
 public class helloBean implements Serializable {
 
-    private String name;
+    private String Program;
+    private Date someDate;
+    private String program;
 
     public helloBean() {
     }
+       
+    private String name;
 
     public String getName() {
         return name;
@@ -30,6 +35,47 @@ public class helloBean implements Serializable {
             return "";
         } else {
             return "Ajax message : Welcome " + name;
+        }
+    }
+    
+    
+    
+    
+    public String getProgram() {
+        return Program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public Date getSomeDate() {
+        return someDate;
+    }
+
+    public void setSomeDate(Date someDate) {
+        this.someDate = someDate;
+    }
+
+    public String getSayProgram() {
+        String outStr = "";
+        //check if null?
+        if ("".equals(program) || program == null) {
+            return "";
+        } else {
+            int num = Integer.parseInt(program);
+            switch (num) {
+                case 1:
+                    outStr = "Programa: Tecnologias para desenvolvimento Web";
+                    break;
+                case 2:
+                    outStr = "Programa: Oficina Certificadora";
+                    break;
+                case 3:
+                    outStr = "Programa: Outra disciplina";
+                    break;
+            }
+            return outStr;
         }
     }
 }
